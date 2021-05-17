e54ffbd8494ea77e32e807bc36e0d183be5bac43
curtis chat <curtis.ch.chat@gmail.com>
2021-05-17 15:33:43 -0600

    add button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,28 @@
 <template>
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{{title}}</h1>
+      <Button text="Add Task" color="green" />
+
     </header>
 </template>
 
 <script>
+import Button from './Button'
+
   export default {
     name: 'Header',
+    props: {
+      title: String,
+    },
+    components: {
+      Button
+    }
   }
 </script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
